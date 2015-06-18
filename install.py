403ac1c01dc676289b2ec57e34320ce7edd6b0e3
Hector Acosta <hector.acosta@gmail.com>
2015-06-18 12:44:52 -0500

    pep8
    Signed-off-by: Hector Acosta <hector.acosta@gmail.com>

--- install.py
+++ install.py
@@ -8,22 +8,26 @@ import sys
 
 WHITELIST = [sys.argv[0], 'install.rb', '.git', '.gitignore']
 
+
 def pre_hook():
     pass
+
 
 def post_hook():
     # Install all git submodules
     subprocess.call('git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle', shell=True)
     subprocess.call('vim +BundleInstall +qall', shell=True)
 
+
 def rm_rf(path):
     assert(os.path.exists(path))
-    if os.path.isfile(path) or  os.path.islink(path):
+    if os.path.isfile(path) or os.path.islink(path):
         os.remove(path)
     elif os.path.isdir(path):
         shutil.rmtree(path)
     else:
         raise Exception('Not a file or directory. Wat do?')
+
 
 def ln_s(source, name, overwrite=False, dryrun=False):
     if not os.path.exists(name) or overwrite:
@@ -37,9 +41,10 @@ def ln_s(source, name, overwrite=False, dryrun=False):
     else:
         sys.stderr.write('%s exists, cowardly refusing to overwrite\n' % name)
 
+
 def do_install(configpath='.', overwrite=False, dryrun=False):
     pre_hook()
-    configs = [os.path.abspath(x) for x in os.listdir(configpath) if not x in WHITELIST]
+    configs = [os.path.abspath(x) for x in os.listdir(configpath) if x not in WHITELIST]
     for i in configs:
         ln_s(i, os.path.join(os.path.expanduser('~'), '.' + os.path.basename(i)), overwrite, dryrun)
     post_hook()
